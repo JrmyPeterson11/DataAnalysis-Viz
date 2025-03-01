17b57f0b268f9b55a22ede08922fdb81bccb89a4
Jeremy Peterson <162064073+JrmyPeterson11@users.noreply.github.com>
2025-03-01 18:55:34 +0100

    Update V2AnalyseSegmentClient.py

--- PythonProjet/V2AnalyseSegmentClient.py
+++ PythonProjet/V2AnalyseSegmentClient.py
@@ -14,6 +14,7 @@ df = pd.read_csv('customer_segmentation.csv')
 #1. Présentation et description des données
 print("1. PRÉSENTATION ET DESCRIPTION DES DONNÉES")
 
+
 # Aperçu des données
 print("Aperçu des données:")
 print(df.head())
